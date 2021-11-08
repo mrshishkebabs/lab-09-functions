332b6765c99dbc8b444a03db438d18adb731c723
Ken Park <xkenxparkx@gmail.com>
2021-11-08 16:49:23 -0500

    Omg factorial nightmare for py and html

---
+++ factorial.py
@@ -0,0 +1,13 @@
+﻿def factorial(n):
+    total = 1
+    for i in range(0, n):
+        total = total * (n - i)
+        print("Current i is: " + str(i))
+        print("Current value of total is: " + str(total))
+    return total
+
+
+userstring = input("Number please ")
+usernum = int(userstring, 10)
+
+print(str(usernum) + "! is " + str(factorial(usernum)))
